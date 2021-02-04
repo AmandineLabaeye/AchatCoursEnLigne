b19c09366e481cad82cb91f0eb3716ff47126bdd
AmandineLabaeye <amandine.labaeye@orange.fr>
2021-02-04 17:01:51 +0100

    STORY 10
    Contenu des cours, affiché en JS complétement, grâce au tableau cours.js, adaptation du code d'ajout
    
    Co-Authored-By: Killian  <38458824+naillik95@users.noreply.github.com>

--- donnees/cours.js
+++ donnees/cours.js
@@ -1,7 +1,7 @@
-const COURS = {
-    1: { id: 1, img: 'ux_ui.jpg', titre: 'UX/UI', prix_initial: 200, prix: 9.99, note: 4, stock: 10 },
-    2: { id: 2, img: 'php_8.png', titre: 'PHP 8', prix_initial: 200, prix: 9.99, note: 3, stock: 10 },
-    3: { id: 3, img: 'react_js.png', titre: 'React JS', prix_initial: 200, prix: 9.99, note: 2, stock: 5 },
-    4: { id: 4, img: 'node_js.jpg', titre: 'Node JS', prix_initial: 200, prix: 9.99, note: 5, stock: 3 },
-    5: { id: 5, img: 'my_sql.png', titre: 'MySQL', prix_initial: 200, prix: 9.99, note: 4, stock: 2 }
-}
+const COURS = [
+    { id: 1, img: 'ux_ui.jpg', titre: 'UX/UI', prix_initial: 200, prix: 9.99, note: 4, stock: 10 },
+    { id: 2, img: 'php_8.png', titre: 'PHP 8', prix_initial: 200, prix: 9.99, note: 3, stock: 10 },
+    { id: 3, img: 'react_js.png', titre: 'React JS', prix_initial: 200, prix: 9.99, note: 2, stock: 5 },
+    { id: 4, img: 'node_js.jpg', titre: 'Node JS', prix_initial: 200, prix: 9.99, note: 5, stock: 3 },
+    { id: 5, img: 'my_sql.png', titre: 'MySQL', prix_initial: 200, prix: 9.99, note: 4, stock: 2 }
+]
--- js/app.js
+++ js/app.js
@@ -1,3 +1,121 @@
+// Fonction qui parcours tout le tableau
+COURS.forEach(function(cours) {
+
+    // Fonction qui affiche les articles complétement en JS
+    creationCarteCours(cours.img, cours.titre, cours.note, cours.prix_initial, cours.prix, cours.stock, cours.id);
+
+});
+
+// Fonction qui crée toutes les cartes des cours disponible en JS grâce au tableau JSON cours
+function creationCarteCours(img, titre, note, prix_initial, prix, stock, id) {
+
+    // J'ai récréer tout l'ancien html avec les mêmes classes identiques
+    let parent = document.querySelector("body .container__cours");
+
+    let cours__article = document.createElement("div");
+
+    cours__article.classList.add("cours__article");
+
+    parent.appendChild(cours__article);
+
+    let cours_img = document.createElement("figure");
+
+    cours_img.classList.add("cours_img");
+
+    cours__article.appendChild(cours_img);
+
+    let cours_img_img = document.createElement("img");
+
+    cours_img_img.src = "img/cours/" + img;
+
+    cours_img.appendChild(cours_img_img);
+
+    let info__article = document.createElement("div");
+
+    info__article.classList.add("info__article");
+
+    cours__article.appendChild(info__article);
+
+    let h4 = document.createElement("h4");
+
+    h4.innerText = titre;
+
+    info__article.appendChild(h4);
+
+    let note_figure = document.createElement("figure");
+
+    note_figure.classList.add("note");
+
+    note_figure.classList.add("m_" + note);
+
+    info__article.appendChild(note_figure);
+
+    let note_img = document.createElement("img");
+
+    note_img.src = "img/rates.png";
+
+    note_figure.appendChild(note_img);
+
+    let paragrapheUn = document.createElement("p");
+
+    info__article.appendChild(paragrapheUn);
+
+    let spanParagrapheUn = document.createElement("span");
+
+    spanParagrapheUn.classList.add("prix");
+
+    spanParagrapheUn.innerText = prix_initial + " €";
+
+    paragrapheUn.appendChild(spanParagrapheUn);
+
+    let spanParagrapheUnDeux = document.createElement("span");
+
+    spanParagrapheUnDeux.classList.add("soldes");
+
+    spanParagrapheUnDeux.innerText = prix + " €";
+
+    paragrapheUn.appendChild(spanParagrapheUnDeux);
+
+    let paragrapheDeux = document.createElement("p");
+
+    paragrapheDeux.innerText = "Disponible: ";
+
+    info__article.append(paragrapheDeux);
+
+    let spanParagrapheDeux = document.createElement("span");
+
+    spanParagrapheDeux.classList.add("stock");
+
+    spanParagrapheDeux.innerText = stock;
+
+    paragrapheDeux.appendChild(spanParagrapheDeux);
+
+    let ajouter_au_panierA = document.createElement("a");
+
+    ajouter_au_panierA.href = "#";
+
+    ajouter_au_panierA.classList.add("ajouter-au-panier");
+
+    ajouter_au_panierA.setAttribute("data-id", id);
+
+    info__article.appendChild(ajouter_au_panierA);
+
+    let ajouter_au_panierI = document.createElement("i");
+
+    ajouter_au_panierI.classList.add("fa");
+
+    ajouter_au_panierI.classList.add("fa-cart-plus");
+
+    ajouter_au_panierA.appendChild(ajouter_au_panierI);
+
+    let spanAjouter_au_panier = document.createElement("span");
+
+    spanAjouter_au_panier.innerText = "Ajouter au panier";
+
+    ajouter_au_panierA.appendChild(spanAjouter_au_panier);
+
+}
+
 // On stock et on récupère le local storage
 let Objet = getLocalStorage();
 
@@ -26,16 +144,19 @@ panier.forEach(function(cours) {
         let idCours = cours.getAttribute("data-id");
 
         // On remonte à l'élément parent du bouton en question
-        let parentPanier = event.target.parentElement;
+        let parentPanier = event.target;
+
+        // Vu que l'on a rajouter un span dans le a on est obligé rechercher le parent du parent
+        let parentPanierElement = parentPanier.parentElement.parentElement;
 
         // On récupére le nom du cours
-        let contenuPanier = parentPanier.getElementsByTagName("h4")[0];
+        let contenuPanier = parentPanierElement.querySelector("h4");
 
         // On récupére le prix en solde
-        let prixCours = parentPanier.querySelector(".soldes");
+        let prixCours = parentPanierElement.querySelector(".soldes");
 
         // On récupérer le solde
-        let stockCours = parentPanier.querySelector(".stock")
+        let stockCours = parentPanierElement.querySelector(".stock")
 
         // Je défini le tableau grâce à la fonction qui récupère le local storage
         let Objet = getLocalStorage();
