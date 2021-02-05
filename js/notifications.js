8c78ea7effc1b00ade84ec65823c18472f9180b8
AmandineLabaeye <amandine.labaeye@orange.fr>
2021-02-05 09:10:11 +0100

    modif couleur
    notif

--- js/notifications.js
+++ js/notifications.js
@@ -1,7 +1,7 @@
 // Fonction de notif, tous se créer ici
 function Notif(msg, duree, px) {
     var notif = document.createElement("div");
-    notif.setAttribute("style", "position:absolute;top:" + px + "%;left:80%;border:1px solid rgba(0,0,0,0.25);padding:1% 5% 1% 5%;box-shadow:0 14px 28px rgba(0,0,0,0.25),0 10px 10px rgba(0,0,0,0.22);margin-right:1%;margin-top:1%");
+    notif.setAttribute("style", "position:absolute;top:" + px + "%;left:80%;border:1px solid rgba(0,0,0,0.25);border-radius:20px;padding:1% 5% 1% 5%;box-shadow:0 14px 28px rgba(0,0,0,0.25),0 10px 10px rgba(0,0,0,0.22);margin-right:1%;margin-top:1%;background-color:green;color:white");
     notif.innerHTML = msg;
     setTimeout(function() {
         notif.parentNode.removeChild(notif);
